828b8317401bf37cba347b2389620281439c3764
Seth Dixon <sfkjbg@gmail.com>
2021-11-21 08:19:13 -0500

    user functionality
    -added flash messaging to the sign_up page
    
    - (email from-group) type='text', b/c type='email' not flashing message
    
    -login/logout

--- web_site/auth.py
+++ web_site/auth.py
@@ -1,37 +1,65 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User, db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in success', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('Email does not exist', category='error')
+
     return render_template("login.html", text='"from" test_text_ (auth.py) (login.html)')
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template('logout.html')
+    logout_user()
+    flash('Logged Out', category='success')
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('first_name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already registered', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("Name must be at least 3 characters", category='error')
         elif len(password1) < 7:
             flash('Password is too short', category='error')
         elif password1 != password2:
             flash("Passwords don't match", category='error')
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account Created", category='success')
+            return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
+
 
 @auth.route('/stocks', methods=['GET', 'POST'])
 def stocks():
